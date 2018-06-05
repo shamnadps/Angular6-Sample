50872544bd2e666c52c3a5194883ce94de083235
Shamnad Shaji <shamnad.shaji@gofore.com>
2018-06-05 21:44:52 +0300

    Added Animations

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { DataService } from "../data.service";
 import { Observable } from "rxjs";
 import { ActivatedRoute } from "@angular/router";
-
 @Component({
   selector: "app-details",
   templateUrl: "./details.component.html",
